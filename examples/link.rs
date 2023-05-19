78690eaaa8b2fff952af39ecbd869b816e165260
ealmloff <evanalmloff@gmail.com>
2023-05-19 14:51:32 +0000

    testing123

--- examples/link.rs
+++ examples/link.rs
@@ -9,7 +9,7 @@ fn app(cx: Scope) -> Element {
     cx.render(rsx! (
         div {
             p {
-                a { href: "http://dioxuslabs.com/", "Default link - links outside of your app" }
+                a { href: "http://.com/", "Default link - links outside of your app" }
             }
             p {
                 a {
